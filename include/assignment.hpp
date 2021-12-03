59a6673d04c9c5b7b5b037cdfd871f2ae6e1776a
Dani Lerner <dlerner97@gmail.com>
2021-12-03 15:33:31 -0500

    Created the Assignment struct according to UML specifications

---
+++ include/assignment.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file assignment.hpp
+ * @author Dani Lerner (dalerner@umd.edu)
+ * @brief Assignment struct declaration
+ * @version 0.1
+ * @date 2021-12-03
+ * 
+ * @copyright Copyright (c) 2021 TBD
+ * 
+ */
+
+#pragma once
+
+#include "./crate.hpp"
+#include <array>
+
+struct Assignment {
+    Crate crate;
+    std::array<double, 2> pos_crate_frame;  // m
+    double height_off_ground;               // m
+};
