b769a6d19dc5fd7f329e6b03b6827e38c9b1427e
Dani Lerner <dlerner97@gmail.com>
2021-12-03 15:48:47 -0500

    Created the SwarmMaster class according to UML specifications

---
+++ include/swarm_master/swarm_master.hpp
@@ -0,0 +1,48 @@
+/**
+ * @file swarm_master.hpp
+ * @author Dani Lerner (dalerner@umd.edu)
+ * @brief SwarmMaster class declaration
+ * @version 0.1
+ * @date 2021-12-03
+ * 
+ * @copyright Copyright (c) 2021 TBD
+ * 
+ */
+
+#pragma once
+
+#include <vector>
+#include <queue>
+#include "../structs/task.hpp"
+#include "../structs/crate.hpp"
+#include "../structs/assignment.hpp"
+
+class SwarmMaster {
+ protected:
+    std::vector<int> assigned_ids;
+    std::queue<Task> task_queue;
+ public:
+    SwarmMaster(/* args */) {}
+    ~SwarmMaster() {}
+
+    /**
+     * @brief Add task to task queue
+     * 
+     */
+    void add_to_task_queue(std::vector<Crate>);
+
+    /**
+     * @brief Assign all robots to designated crates
+     * 
+     * @return std::vector<Assignment> 
+     */
+    std::vector<Assignment> assign_robot_to_crate();
+
+    /**
+     * @brief Grab next task from task queue and get it done.
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool perform_next_task();
+};
