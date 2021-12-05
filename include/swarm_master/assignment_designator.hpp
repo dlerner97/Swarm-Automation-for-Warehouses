69c6d0e0bcca320853e4877e0afc8424b82f3d1e
Dani Lerner <dlerner97@gmail.com>
2021-12-05 12:40:43 -0500

    Moved some declarations from these files

--- include/swarm_master/assignment_designator.hpp
+++ include/swarm_master/assignment_designator.hpp
@@ -15,49 +15,8 @@
 #include <vector>
 #include <memory>
 #include <unordered_map>
-
-struct Robot {
-    int id;
-    std::array<double, 2> pos;
-    bool assigned = false;
-    Robot(int _id, std::array<double, 2>& _pos) {
-        this->id = _id;
-        this->pos = _pos;
-    }
-};
-
-struct RobotDist {
-    int robot_id;
-    double dist_sq;
-    RobotDist(int _id, double _dist) {
-        this->robot_id = _id;
-        this->dist_sq = _dist;
-    }
-};
-
-bool operator<(const RobotDist& d1, const RobotDist& d2) {
-    return d1.dist_sq < d2.dist_sq;
-}
-
-class Site {
- public:
-    int site_id;
-    int robots_required;
-    std::array<double, 2> pos;
-    std::vector<int> assigned_ids{};
-    std::vector<RobotDist> dist_to_robots{};
-
-    Site(int _site_id, int _robots_req, std::array<double, 2>& _pos) {
-        this->site_id = _site_id;
-        this->robots_required = _robots_req;
-        this->pos = _pos;
-    }
-    void populate_robot_dists(std::unordered_map<int, Robot>& robots);
-
-    std::vector<RobotDist> get_n_closest(int n);
-
-    std::vector<RobotDist> find_all_lt_dist(double dist);
-};
+#include "../structs/robot.hpp"
+#include "../structs/site.hpp"
 
 class AssignmentDesignator {
  protected:
--- src/swarm_master/assignment_designator.cpp
+++ src/swarm_master/assignment_designator.cpp
@@ -17,27 +17,6 @@
 #include <unordered_map>
 #include "../../include/swarm_master/assignment_designator.hpp"
 
-void Site::populate_robot_dists(std::unordered_map<int, Robot>& robots) {
-    for (const auto& robot : robots) {
-        double dist_sq = pow(pos[0], robot.second.pos[0]) + pow(pos[1], robot.second.pos[1]);
-        dist_to_robots.push_back({robot.first, dist_sq});
-    }
-    std::sort(dist_to_robots.begin(), dist_to_robots.end());
-}
-
-std::vector<RobotDist> Site::get_n_closest(int n) {
-    return std::vector<RobotDist>(dist_to_robots.begin(), dist_to_robots.end()+n);
-}
-
-std::vector<RobotDist> Site::find_all_lt_dist(double dist) {
-    int index = 0;
-    for (std::size_t i=0; i < dist_to_robots.size(); i++) {
-        if (dist_to_robots[i].dist_sq > dist) break;
-        index = i;
-    }
-    return get_n_closest(index);
-}
-
 GrowingRadiusDesignator::SiteVec GrowingRadiusDesignator::get_designations() {
     SiteVec ret = std::make_shared<std::vector<Site>>(std::vector<Site>{});
     int total_robots_required{0};
