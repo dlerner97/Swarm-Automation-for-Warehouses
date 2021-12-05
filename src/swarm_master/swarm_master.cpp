45566388fdb423f37ea2d506e781a9e78b5d4676
Dani Lerner <dlerner97@gmail.com>
2021-12-05 12:29:56 -0500

    Created swarm_master exe file. Unimplemented

---
+++ src/swarm_master/swarm_master.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file swarm_master.cpp
+ * @author Dani Lerner (dalerner@umd.edu)
+ * @brief SwarmMaster class executable
+ * @version 0.1
+ * @date 2021-12-04
+ * 
+ * @copyright Copyright (c) 2021 TBD
+ * 
+ */
+
+#include <vector>
+#include "../../include/swarm_master/swarm_master.hpp"
+#include "../../include/swarm_master/assignment_designator.hpp"
