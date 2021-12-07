9d463fde06af5c1dd787ab3ed882f05126fa873e
Dani Lerner <dlerner97@gmail.com>
2021-12-06 21:39:04 -0500

    Added swarm_master main to repo

---
+++ src/swarm_master/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author Dani Lerner (dalerner@umd.edu)
+ * @brief Main script for RosSwarmMaster
+ * @version 0.1
+ * @date 2021-12-06
+ * 
+ * @copyright Copyright (c) 2021 TBD
+ * 
+ */
+
+#include "../../include/swarm_master/ros_swarm_master.hpp"
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "swarmMaster");
+    RosSwarmMaster master;
+    return 0;
+}
