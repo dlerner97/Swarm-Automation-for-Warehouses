89259908bcec2da31c865930a9a3bb159c6d4421
Dani Lerner <dlerner97@gmail.com>
2021-12-09 16:39:37 -0500

    Changed spacing in ros swarm master hpp file

--- include/swarm_master/ros_swarm_master.hpp
+++ include/swarm_master/ros_swarm_master.hpp
@@ -24,55 +24,55 @@
 
 class RosSwarmMaster {
  private:
-    SwarmMaster master;  
-    ros::NodeHandle nh;
-    ros::ServiceServer swarm_connect_server;
-    ros::Subscriber swarm_task_subscriber;
+  SwarmMaster master;  
+  ros::NodeHandle nh;
+  ros::ServiceServer swarm_connect_server;
+  ros::Subscriber swarm_task_subscriber;
 
-    std::string robot_namespace_begin;
-    std::string task_server_name;
+  std::string robot_namespace_begin;
+  std::string task_server_name;
 
-    std::unordered_map<int, ros::Subscriber> all_pos_subscriber;
-    std::unordered_map<int, ros::Publisher> all_task_publisher;
-    std::unordered_map<int, ros::Publisher> all_site_ready_pub;
-    std::unordered_map<int, ros::Subscriber> robot_site_waiting_pub;
+  std::unordered_map<int, ros::Subscriber> all_pos_subscriber;
+  std::unordered_map<int, ros::Publisher> all_task_publisher;
+  std::unordered_map<int, ros::Publisher> all_site_ready_pub;
+  std::unordered_map<int, ros::Subscriber> robot_site_waiting_pub;
 
-    /**
-      * @brief Get task callback function for swarm task service server
-      * 
-      */
-    void get_task_callback(warehouse_swarm::Crate::ConstPtr& crate);
+  /**
+    * @brief Get task callback function for swarm task service server
+    * 
+    */
+  void get_task_callback(warehouse_swarm::Crate::ConstPtr& crate);
 
-    /**
-      * @brief Swarm connect callback function for swarm connect service server
-      * 
-      */
-    bool swarm_connect_callback(warehouse_swarm::SwarmConnect::Request& req,
-                                warehouse_swarm::SwarmConnect::Response& resp);
+  /**
+    * @brief Swarm connect callback function for swarm connect service server
+    * 
+    */
+  bool swarm_connect_callback(warehouse_swarm::SwarmConnect::Request& req,
+                              warehouse_swarm::SwarmConnect::Response& resp);
 
-    /**
-      * @brief Robot waiting topic callback
-      * 
-      * @param robot_id 
-      */
-    void get_robot_waiting_callback(std_msgs::UInt16::ConstPtr& robot_id);
+  /**
+    * @brief Robot waiting topic callback
+    * 
+    * @param robot_id 
+    */
+  void get_robot_waiting_callback(std_msgs::UInt16::ConstPtr& robot_id);
 
  public:
-    RosSwarmMaster(double _weight_per_robot=2.0) : 
-            master{_weight_per_robot} {
-        ROS_INFO_STREAM("Spinning up RosSwarmMaster.");
-        swarm_connect_server = nh.advertiseService(
-          "swarm_connect", &RosSwarmMaster::swarm_connect_callback, this);
-        swarm_task_subscriber = nh.subscribe("payload_details", 100,
-            &RosSwarmMaster::get_task_callback, this);
-    }
-    ~RosSwarmMaster() {
-        ROS_INFO_STREAM("Shutting down RosSwarmMaster.");
-    }
+  RosSwarmMaster(double _weight_per_robot=2.0) : 
+          master{_weight_per_robot} {
+      ROS_INFO_STREAM("Spinning up RosSwarmMaster.");
+      swarm_connect_server = nh.advertiseService(
+        "swarm_connect", &RosSwarmMaster::swarm_connect_callback, this);
+      swarm_task_subscriber = nh.subscribe("payload_details", 100,
+          &RosSwarmMaster::get_task_callback, this);
+  }
+  ~RosSwarmMaster() {
+      ROS_INFO_STREAM("Shutting down RosSwarmMaster.");
+  }
 
-    /**
-      * @brief Call SwarmMaster assign_robots_to_crate
-      * 
-      */
-    void assign_robots();
+  /**
+    * @brief Call SwarmMaster assign_robots_to_crate
+    * 
+    */
+  void assign_robots();
 };
